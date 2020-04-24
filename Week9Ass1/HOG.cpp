54f8774799eb444b7bf0028a5608f9801e490e07
CHUJUN <10094@LAPTOP-P2UL3LSA>
2020-04-24 16:26:26 +0800

    correct Week9Ass with some detail error, and add some note

--- Week9Ass1/HOG.cpp
+++ Week9Ass1/HOG.cpp
@@ -15,7 +15,7 @@ int main() {
 	int blockSize = 16;
 	int nX = refMat.cols / blockSize;
 	int nY = refMat.rows / blockSize;
-	int bins = 8*nX*nY;
+	int bins = 9*nX*nY;
 
 	float * ref_hist = new float[bins];
 	float * test1_hist = new float[bins];
@@ -29,15 +29,20 @@ int main() {
 	draw_His(test_2, blockSize, nX, nY, test2_hist);
 	float O_Dis1 = Euclid_Distance(ref_hist, test1_hist, bins);
 	float O_Dis2 = Euclid_Distance(ref_hist, test2_hist, bins);
-	cout << "Euclid_Distance of Pic1 " << O_Dis1 << endl;
-	cout << "Euclid_Distance of Pic2 " << O_Dis2 << endl;
+	delete[] ref_hist, test1_hist, test2_hist;
+	cout << "Euclid_Distance of Pic1 " << (int)O_Dis1 << endl;
+	cout << "Euclid_Distance of Pic2 " << (int)O_Dis2 << endl;
 	//for (int i = 0;i < bins;i++) {
 	//	cout << i<<":"<<ref_hist[i] << endl;
 	//}
-	delete[] ref_hist, test1_hist, test2_hist;
 	return 0;
-}
+	/* the result is :
+	Euclid_Distance of Pic1 188215776
+	Euclid_Distance of Pic2 373521952
+	Obviously ,Pic1 is closer to orignal picture! */
 
+}
+//----------Function to draw  histogram--------//
 void draw_His(Mat & src, int blockSize, int nX,int nY, float * hist) {
 	Mat gx, gy;
 	Mat mag, angle;
@@ -48,15 +53,19 @@ void draw_His(Mat & src, int blockSize, int nX,int nY, float * hist) {
 	for (int i = 0; i < nY;i++) {
 		for (int j = 0; j < nX;j++) {
 			cal_His(mag, angle, blockSize, hist, j, i, bias);
-			bias += 8;
+			bias += 9;
 		}
 	}
 }
-
+/*----------draw histogram blocks by blocks--------
+use biasX\biasY to calculate bias position of blocks
+use bias to calculate bias of histogram tuple:
+			*(hist + range + bias)               */
 void cal_His(Mat & mag, Mat & angle, int blockSize,float * hist,int biasX, int biasY,int bias) {
-	for (int i = biasX * blockSize;i < blockSize + biasX * blockSize;i++) {
-		for (int j = biasY * blockSize;j < blockSize + biasY * blockSize;j++) {
-			int range = angle.at<float>(i, j) / 40;
+	int bias_PicX = biasX * blockSize, bias_PicY = biasY * blockSize;
+	for (int i = bias_PicX;i < blockSize + bias_PicX;i++) {
+		for (int j = bias_PicY;j < blockSize + bias_PicY;j++) {
+			int range = (angle.at<float>(i, j) -1) / 40;  //control range from 0-8
 			*(hist + range + bias) += mag.at<float>(i, j);
 		}
 	}
